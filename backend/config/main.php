69bd7727b7dbde2d2d762604343b3cda07da5dab
vlafo <afonin006@gmail.com>
2017-12-19 16:16:47 +0300

    add stylish theme with composer & in config

--- backend/config/main.php
+++ backend/config/main.php
@@ -14,6 +14,13 @@ return [
     'modules' => [],
     'homeUrl' => '/admin',
     'components' => [
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+                ]
+            ]
+        ],
         'request' => [
             'csrfParam' => '_csrf-backend',
             'baseUrl' => '/admin',
